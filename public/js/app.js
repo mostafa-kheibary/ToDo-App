ea6220be93b9a5b37787890be348b5ab3ded3f4f
mostafa kheibary <mostafamnbvcxz5@gmail.com>
2021-11-15 23:44:08 +0330

    add form submit and add comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+// our constant
 const AddBot = document.querySelector(".add-bott");
 const Container2 = document.querySelector(".container-2");
 const Close = document.querySelector(".close");
@@ -6,14 +7,23 @@ const Menu = document.querySelector(".menu");
 const Topic = document.getElementById("topic");
 const ExContent = document.getElementById("content");
 const CloseLi = document.querySelector(".closeli");
+const form = document.querySelector("form")
 
 
+// page Navigation 
 AddBot.addEventListener("click", () => {
     Container2.classList.add("show-cont2")
 });
 Close.addEventListener("click", function () {
     Container2.classList.remove("show-cont2")
 });
+
+// prevent to reload page when form submit
+form.addEventListener("submit",function(e){
+e.preventDefault();
+});
+
+// click event for button
 Submit.addEventListener("click", function () {
     Container2.classList.remove("show-cont2")
     if (Topic.value != `` | ExContent.value != ``) {
@@ -33,6 +43,3 @@ Submit.addEventListener("click", function () {
 });
 
 
-
-
-// `<li><div><h4>${Topic.value}</h4><p>${ExContent.value}<p></div><div><i class="fas fa-times"></i></div></li>`
