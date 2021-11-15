dd5b3171a7f0b9cd9c2230e41893e3fb655302b7
mostafa kheibary <mostafamnbvcxz5@gmail.com>
2021-11-15 23:34:10 +0330

    fix bug and add Delete button

--- public/js/app.js
+++ public/js/app.js
@@ -5,27 +5,32 @@ const Submit = document.getElementById("submit");
 const Menu = document.querySelector(".menu");
 const Topic = document.getElementById("topic");
 const ExContent = document.getElementById("content");
-const ToDos = [];
+const CloseLi = document.querySelector(".closeli");
 
-AddBot.addEventListener("click",()=>{
+
+AddBot.addEventListener("click", () => {
     Container2.classList.add("show-cont2")
 });
-Close.addEventListener("click",function(){
+Close.addEventListener("click", function () {
     Container2.classList.remove("show-cont2")
 });
-Submit.addEventListener("click",function(){
+Submit.addEventListener("click", function () {
     Container2.classList.remove("show-cont2")
-    
-    if(Topic.value != ``|ExContent.value != ``){
-       ToDos.push(Topic.value);
-       for(const todo of ToDos){
-            let text = `<li><div><h4>${todo}</h4><p>${todo}<p></div><div><i class="fas fa-times"></i></div></li>`;
-           Menu.innerHTML += text;
-       }
-       
+    if (Topic.value != `` | ExContent.value != ``) {
+        Menu.innerHTML += `<li><div><h4>${Topic.value}</h4><p>${ExContent.value}<p></div><div><i class="fas fa-times closeli"></i></div></li>`;
+        Topic.value = "";
+        ExContent.value = "";
     }
-});
+    const lis = Menu.children;
+    for (const li of lis) {
+        li.addEventListener("click", function (e) {
+            if (e.target.tagName == "I") {
+                li.remove();
+            }
+        });
+    }
 
+});
 
 
 
