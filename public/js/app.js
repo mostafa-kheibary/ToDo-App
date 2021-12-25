b87f2a0c04bc83fd9c476d431b3c94a0970681d6
mostafa <mostafamnbvcxz5@gmail.com>
2021-12-25 22:18:02 +0330

    chage js code and make it more cleaner, change style and add more functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,89 +1,187 @@
-// our var
-const search = document.getElementById("search");
-const AddBot = document.querySelector(".add-bott");
-const Container2 = document.querySelector(".container-2");
-const Close = document.querySelector(".close i");
-const Submit = document.getElementById("submit");
-const Menu = document.querySelector(".menu");
-const Topic = document.getElementById("topic");
-const CloseLi = document.querySelector(".closeli");
-const form = document.querySelector("form")
-const body = document.querySelector('html');
-const ErrorBox = document.querySelector('.error');
-const ErrorMessage = document.querySelector('.message');
-const Todos = [];
-let html = '';
-// page Navigation 
-AddBot.addEventListener("click", () => {
-    Container2.classList.add("show-cont2")
-});
-Close.addEventListener("click", function () {
-    Container2.classList.remove("show-cont2")
-});
+// constant and varible :
+const TodoInput = document.querySelector(".todo-input");
+const Todoslist = document.querySelector(".todo-list");
+const CreateButton = document.querySelector(".submit");
+const Page = document.querySelector(".container-2");
+const OpenPage = document.querySelector(".add-botton");
+const ClosePage = document.querySelector(".close-page");
+const toast = document.querySelector(".message");
+const RemoveAllButton = document.querySelector(".remove-all");
+const SearchInput = document.querySelector(".search-input");
+const SortButtons = document.querySelectorAll(".sort");
+const pre = document.querySelector(".pre");
 
-
-// prevent to reload page when form submit
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
+// event :
+OpenPage.addEventListener("click", Open);
+ClosePage.addEventListener("click", Close)
+CreateButton.addEventListener("click", () => {
+    if (TodoInput.value.trim() !== "") {
+        Close();
+    }
+    AddTodo();
 });
-// click event for submit button 
-Submit.addEventListener("click", function () {
-    Container2.classList.remove("show-cont2");
-    if (Topic.value.trim() != ``) {
-        // TODOS array
-        Todos.push(Topic.value);
-        let html = '';
-        // for get all item from todos arrey
-        for (const todo of Todos) {
-            html += `<li><div><h4>${todo}</h4></div><div class= "split"><i class="fas fa-times" id="closeli"></i><i class="fas fa-check" id="done"></i></div></li>`;
-        }
-        console.log(Todos);
-        Menu.innerHTML = html;
+Todoslist.addEventListener("click", RemoveTodo);
+toast.addEventListener("click", ToastRemove);
+TodoInput.addEventListener("keydown", ToastRemove);
+RemoveAllButton.addEventListener("click", RemoveAll);
+SearchInput.addEventListener("keyup", SearchTodo);
+for (const button of SortButtons) {
+    const key = button.innerText;
+    button.addEventListener("click", function () {
+        Sort(key)
+    })
+}
+
+// function :
+function Open() {
+    Page.classList.add("show-cont2");
+}
+function Close() {
+    Page.classList.remove("show-cont2");
+}
+// adding todo 
+function AddTodo() {
+    event.preventDefault();
+    SaveLocal(TodoInput.value);
+    if (TodoInput.value.trim() !== "") {
+        const TodoElement = `<li><div><h4>${TodoInput.value}</h4></div><div class= "split"><i class="far fa-trash-alt" id="closeli"></i><i class="fas fa-check" id="done"></i></div></li>`;
+        Todoslist.innerHTML += TodoElement;
+        // show delete all button when first todo was create
+        RemoveAllButton.classList.add("show-remove-all");
+        pre.style.display = "none";
     }
     else {
-        ErrorMessage.classList.add('toast-animation');
-        setTimeout(() => ErrorMessage.classList.remove('toast-animation'), 3000);
+        ToastShow();
     }
-    // clean the input value
-    Topic.value = '';
-
-    // deliting li part
-    const lis = Menu.children;
-    for (const li of lis) {
-        li.addEventListener("click", function (e) {
-            if (e.target.id == "closeli") {
-                // find index of li to remove it
-                const x = Todos.findIndex((n) => n == li.innerText);
-                // splice it from arrey and remove
-                Todos.splice(x, 1);
-                console.log(Todos);
-                //  (delete li) animataion
-                li.classList.add("li-detroy");
-                li.style = 'height: 0px;margin: 0px;padding:0px;'
-                setTimeout(function () {
-                    li.remove();
-                    li.classList.remove("li-detroy");
-                }, 700);
+    TodoInput.value = "";
+}
+// removing and check the todo
+function RemoveTodo(e) {
+    if (e.target.id === "closeli") {
+        const myTodo = e.target.parentElement.parentElement;
+        myTodo.classList.add("close-todo");
+        DeleteTodo(myTodo);
+        setTimeout(() => {
+            myTodo.remove();
+            // delete the button when we dont have any todo
+            if (Todoslist.innerHTML == "") {
+                RemoveAllButton.classList.remove("show-remove-all");
+                pre.style.display = "block";
             }
-            if(e.target.id == "done"){
-                li.classList.toggle('todo-done');
-            }
-        });
+        }, 500);
     }
-});
-// when start wrinthing toast will be disappre
-Topic.addEventListener('keydown', () => ErrorMessage.classList.remove('toast-animation'));
-ErrorMessage.addEventListener('click', () => ErrorMessage.classList.remove('toast-animation'));
-// search option
-const lis = Menu.children;
-search.addEventListener("keyup", function () {
-    const searchVall = search.value.trim();
-    for (const li of lis) {
-        if (li.innerText.includes(searchVall)) {
-            li.style.display = "flex";
+    if (e.target.id === "done") {
+        const myTodo = e.target.parentElement.parentElement;
+        myTodo.classList.toggle("done-todo");
+        let Todos;
+        if (localStorage.getItem("Todos") == null) {
+            Todos = [];
         }
         else {
-            li.style.display = "none";
+            Todos = JSON.parse(localStorage.getItem("Todos"))
+        }
+        for (const todo of Todos) {
+            if (todo.isdone == false) {
+                todo.isdone = true;
+            }
+            else {
+                todo.isdone = false;
+            }
+        }
+        localStorage.setItem("Todos", JSON.stringify(Todos));
+    }
+}
+// remove all todo 
+
+function RemoveAll() {
+    for (const todo of Todoslist.children) {
+        DeleteTodo(todo.innerText)
+        todo.classList.add("close-all-todo");
+        setTimeout(() => todo.remove(), 500);
+    }
+    setTimeout(() => {
+        // delete the button when we dont have any todo
+        if (Todoslist.innerHTML == "") {
+            RemoveAllButton.classList.remove("show-remove-all");
+            pre.style.display = "block";
+        }
+    }, 500);
+}
+function SearchTodo() {
+    const Todos = Todoslist.children;
+    const value = SearchInput.value.trim();
+    for (const todo of Todos) {
+        if (todo.innerText.includes(value)) {
+            todo.style.transform = "scale(1)";
+            todo.style.height = "90px";
+            todo.style.margin = "30px";
+            todo.style.padding = "25px";
+            setTimeout(() => todo.style.display = "flex", 300);
+        }
+        else {
+            todo.style.transform = "scale(0)";
+            todo.style.height = "0";
+            todo.style.margin = "0";
+            todo.style.padding = "0";
+            setTimeout(() => todo.style.display = "none", 300);
         }
     }
-});
+}
+// show the Error message
+function ToastShow() {
+    toast.classList.add("toast-animation");
+    setTimeout(() => toast.classList.remove("toast-animation"), 3000);
+}
+function ToastRemove() {
+    toast.classList.remove("toast-animation");
+}
+// sort todo
+function Sort(cat) {
+
+}
+// local storage save
+function SaveLocal(todo) {
+    let Todos;
+    if (localStorage.getItem("Todos") == null) {
+        Todos = [];
+    }
+    else {
+        Todos = JSON.parse(localStorage.getItem("Todos"))
+    }
+    if (todo.trim() !== "") {
+        Todos.push({ title: todo.trim(), isdone: false });
+        localStorage.setItem("Todos", JSON.stringify(Todos));
+    }
+
+
+}
+function DeleteTodo(todo) {
+    let Todos;
+    if (localStorage.getItem("Todos") == null) {
+        Todos = [];
+    }
+    else {
+        Todos = JSON.parse(localStorage.getItem("Todos"))
+    }
+    const index = Todos.indexOf(todo.innerText);
+    Todos.splice(index, 1);
+    localStorage.setItem("Todos", JSON.stringify(Todos));
+}
+function GetTodo() {
+    let Todos;
+    if (localStorage.getItem("Todos") == null) {
+        Todos = [];
+    }
+    else {
+        Todos = JSON.parse(localStorage.getItem("Todos"))
+    }
+    for (const todo of Todos) {
+        const TodoElement = `<li><div><h4>${todo.title}</h4></div><div class= "split"><i class="far fa-trash-alt" id="closeli"></i><i class="fas fa-check" id="done"></i></div></li>`;
+        Todoslist.innerHTML += TodoElement;
+        pre.style.display = "none";
+    }
+    if (Todoslist.innerHTML !== "") {
+        RemoveAllButton.classList.add("show-remove-all");
+    }
+}
+GetTodo();
